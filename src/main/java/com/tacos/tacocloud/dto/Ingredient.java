93eba55e692ca84a2699cf1b7ed6ac18a33ee346
Oleksandr Ivanitskyy <oleksandr.ivanitskyy@glovoapp.com>
2022-01-13 13:30:57 +0100

    Taco design implemented

---
+++ src/main/java/com/tacos/tacocloud/dto/Ingredient.java
@@ -0,0 +1,18 @@
+package com.tacos.tacocloud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data 
+@AllArgsConstructor
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+}
